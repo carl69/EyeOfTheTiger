2544a4d6cce8c6e8598c283cb3cd74f0d14875c5
carl69 <carlmorgenstierne@yahoo.com>
2017-09-13 13:51:45 +0200

    made own scene
    made my self a test scene

--- SPO2/Assets/Scripts/Player/playerStats.cs
+++ SPO2/Assets/Scripts/Player/playerStats.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerStats : MonoBehaviour {
-    public float playerSpeed, jumphight, gravity, maxHunger = 100f;
+    public string InfoMovement = "Player movement script";
+    public float playerSpeed, jumphight, gravity;
+    public string InfoHunger = "Player Hunger script";
+    public float maxHunger = 100f, hungerSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
