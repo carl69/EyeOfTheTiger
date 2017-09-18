5bea3ae76e76fb378efadb5f083820803bb1b914
Luismu02 <luismu02@ucm.es>
2017-09-18 14:06:43 +0200

    do the water

---
+++ SPO2/Assets/Scripts/Player/food.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour {
+	public int eaten = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "food") {
+			eaten++;
+			Destroy (other.gameObject);
+		}
+
+	}
+
+	void OnGUI (){
+		GUILayout.Label ("food =" + eaten);
+	}
+}
---
+++ SPO2/Assets/Scripts/Player/water.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour {
+	public bool drinking = false;
+	public int drink = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		if (drinking == true){
+			drink++;
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "water") {
+			drinking = true;
+		}
+	}
+
+
+	public void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "water") {
+			drinking = false;
+		}
+	}
+
+	void OnGUI (){
+		GUILayout.Label ("");
+		GUILayout.Label ("water =" + drink);
+	}
+}
+
