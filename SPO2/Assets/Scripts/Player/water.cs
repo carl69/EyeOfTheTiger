b735b5b6694792b7ecc4cdf80a6b8a588d675b24
carl69 <carlmorgenstierne@yahoo.com>
2017-09-18 15:04:05 +0200

    added cap on water. added water drain

--- SPO2/Assets/Scripts/Player/water.cs
+++ SPO2/Assets/Scripts/Player/water.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class water : MonoBehaviour {
 	public bool drinking = false;
-	public int drink = 0;
+	public int drink = 100;
 
+    private float rate = 2;
+    public float timer;
 	// Use this for initialization
 	void Start () {
 
 	}
 
-	void Update () {
-		if (drinking == true){
-			drink++;
-		}
-	}
+    void Update() {
+
+        if (drinking == true)
+        {
+            if (drink < 200)
+            {
+                drink++;
+            }
+        }
+        else if (timer < Time.time)
+        {
+            timer = Time.time + rate;
+            drink--;
+        }
+        
+    }
 
 	public void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "water") {
