494664ef3303fea15b5e5135c8c2b5ec105835a3
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-09-20 15:23:10 +0200

    Updates to Jon testing folder. Foosteps audio and script testing.

---
+++ SPO2/Assets/Jon/Audio/SFX/Footsteps.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    public AudioClip footstep;
+
+    public bool clipPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) && (clipPlaying == false) && (GetComponent<AudioSource>().isPlaying == false))
+        {
+            GetComponent<AudioSource>().PlayOneShot(footstep);
+           // clipPlaying = true;
+        }
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            clipPlaying = false;
+        }
+        
+        }
+
+        
+       
+       
+           
+        
+                
+                     
+
+	
+}
