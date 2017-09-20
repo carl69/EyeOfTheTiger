923d666f9a1abdd6e54a173ac3ecb9922ee229f7
carl69 <carlmorgenstierne@yahoo.com>
2017-09-20 16:54:50 +0200

    Hive for the cub

---
+++ SPO2/Assets/CubPickUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubPickUp : MonoBehaviour {
+    bool canPickUp = false;
+    bool pickedUp = false;
+    bool inHive = false;
+    public Transform holdingPosision;
+    private Transform hiveLocal;
+    
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if (canPickUp && Input.GetKeyDown(KeyCode.E))
+        {
+            pickedUp = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            pickedUp = false;
+        }
+
+        if (pickedUp)
+        {
+            transform.position = holdingPosision.position;
+        }
+        else {
+        }
+
+        if (inHive && !pickedUp)
+        {
+            transform.position = hiveLocal.position;
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.tag =="Player")
+        {
+
+            canPickUp = true;
+        }
+        if (c.tag == "Hive")
+        {
+            hiveLocal = c.transform;
+            inHive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.tag == "Player")
+        {
+            canPickUp = false;
+        }
+        if (c.tag == "Hive")
+        {
+            inHive = false;
+        }
+    }
+}
