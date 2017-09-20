9ef58a589554e3f1566e6e2480a06981d68fcb4c
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-09-20 19:40:24 +0200

    Audio update. Import of audio files into Jon Test folder. Audio script creation and AudioController setup. Basic sounds implemented.

---
+++ SPO2/Assets/Jon/Audio/SFX/Audio_pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_pickup : MonoBehaviour {
+
+    public AudioClip food;
+    public AudioClip water;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "food")
+        {
+            GetComponent<AudioSource>().PlayOneShot(food);
+        }
+        else if (other.gameObject.tag == "water")
+        {
+            GetComponent<AudioSource>().PlayOneShot(water);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
