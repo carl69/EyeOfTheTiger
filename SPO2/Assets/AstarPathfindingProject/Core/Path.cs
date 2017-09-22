522a45d19b620448c3038ed828a2516a1288a45b
carl69 <carlmorgenstierne@yahoo.com>
2017-09-22 15:31:11 +0200

    something

--- SPO2/Assets/AstarPathfindingProject/Core/Path.cs
+++ SPO2/Assets/AstarPathfindingProject/Core/Path.cs
@@ -379,7 +379,7 @@ namespace Pathfinding {
 #endif
 
 			if (AstarPath.active.logPathResults != PathLog.None && AstarPath.active.logPathResults != PathLog.InGame) {
-				Debug.LogWarning(msg);
+				//Debug.LogWarning(msg);
 			}
 		}
 
