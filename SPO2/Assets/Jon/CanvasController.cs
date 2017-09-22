433ad6aa4169b00cfbe8291ab6678917ad099249
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-09-22 21:03:33 +0200

    Added a working system for showing text prompts in canvas gameobject. Added some new tags to this end as well.

---
+++ SPO2/Assets/Jon/CanvasController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour {
+
+    public GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+   public void Continue()
+    {
+        Debug.Log("Button clicked!");
+        Time.timeScale = 1;
+        gameObject.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(false);  
+        textBox.SetActive(false);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SPO2/Assets/Jon/TextPrompt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TextPrompt : MonoBehaviour {
+    
+
+    public string triggerNumber;
+
+    public GameObject textBox;
+   // public CanvasTest boolToCheck;
+
+    public bool text1Shown = false;
+    public bool text2Shown = false;
+
+	// Use this for initialization
+	void Start () {
+
+       
+
+        	
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && triggerNumber == "1" && text1Shown == false)
+        {
+            textBox.SetActive(true);
+            textBox.transform.GetChild(0).gameObject.SetActive(true);
+            text1Shown = true;
+            Time.timeScale = 0;
+
+        }
+        if (collision.gameObject.tag == "Player" && triggerNumber == "2" && text2Shown == false)
+        {
+            textBox.SetActive(true);
+            textBox.transform.GetChild(1).gameObject.SetActive(true);
+            text2Shown = true;
+            Time.timeScale = 0;            
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
