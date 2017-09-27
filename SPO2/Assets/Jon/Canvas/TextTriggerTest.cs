371faada2812b2255897565f0340418e56846c0f
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-09-27 13:53:58 +0200

    Testing out new canvas script in my scene.

---
+++ SPO2/Assets/Jon/Canvas/TextTriggerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTriggerTest : MonoBehaviour {
+    public GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+
+        textBox = GameObject.FindGameObjectWithTag("TextPrompts");
+	}
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            textBox.transform.GetChild(0).gameObject.SetActive(true);
+            
+            textBox.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "New text written here in code";
+                     
+            Time.timeScale = 0;
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
