ac9b0a3f217c5e28121a1ef7ec6c3bff7c2a9638
carl69 <carlmorgenstierne@yahoo.com>
2017-09-27 16:31:19 +0200

    made the hive and pick up script work on W and you have to carry the cub to the hive and place him down on it

--- SPO2/Assets/Scripts/AI/Cub/CubPickUp.cs
+++ SPO2/Assets/Scripts/AI/Cub/CubPickUp.cs
@@ -5,70 +5,93 @@ using UnityEngine;
 public class CubPickUp : MonoBehaviour {
     bool canPickUp = false;
     bool pickedUp = false;
-    bool inHive = false;
+    public bool inHive = false;
+    bool canHive = false;
     public Transform holdingPosision;
     private Transform hiveLocal;
 
+    GameObject curHive;
+    GameObject curplayer;
     CubMovement cubMovement;
 	// Use this for initialization
 	void Start () {
         cubMovement = this.gameObject.GetComponent<CubMovement>();
-
-
+        curplayer = GameObject.Find("CubHoldingPosition");
+        holdingPosision = curplayer.transform;
 	}
 
     // Update is called once per frame
     void Update() {
-
-
-        if (canPickUp && Input.GetKeyDown(KeyCode.E))
-        {
-            pickedUp = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log(pickedUp);
-            pickedUp = false;
-            
-        }
-
         if (pickedUp)
         {
             transform.position = holdingPosision.position;
-            //cubMovement.enabled = false;
-        }
-        else {
-        }
-
-        if (inHive && !pickedUp)
-        {
-            transform.position = hiveLocal.position;
             cubMovement.enabled = false;
         }
+        if (inHive)
+        {
+            Debug.Log("Hive");
+            HiveMode();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (!pickedUp && canPickUp)
+            {
+                Debug.Log("Pick up");
+                pickedUp = true;
+                inHive = false;
+            }
+            else if (pickedUp && canHive)
+            {
+                Debug.Log("Put In The Hive");
+                inHive = true;
+                pickedUp = false;
+            }
+            else if (pickedUp)
+            {
+                Debug.Log("Put down");
+                cubMovement.enabled = true;
+                pickedUp = false;
+            }
+
+            
+            
+
+            
+
+        }
 	}
+    void HiveMode() {
+        hiveLocal = curHive.transform;
+        transform.position = new Vector3(hiveLocal.position.x, hiveLocal.position.y, 0);
+        cubMovement.enabled = false;
+    }
     private void OnTriggerEnter(Collider c)
     {
         if (c.tag =="Player")
         {
-
+            Debug.Log("In  Pick up range");
             canPickUp = true;
         }
         if (c.tag == "Hive")
         {
-            hiveLocal = c.transform;
-            inHive = true;
+            Debug.Log("In Hive range");
+            curHive = c.gameObject;
+            canHive = true;
         }
     }
     private void OnTriggerExit(Collider c)
     {
-        cubMovement.enabled = true;
         if (c.tag == "Player")
         {
+            Debug.Log("Out of pick up range");
             canPickUp = false;
         }
         if (c.tag == "Hive")
         {
-            inHive = false;
+            Debug.Log("Out of Hive range");
+            canHive = false;
         }
+        
     }
 }
