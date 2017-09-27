c3d13f22d3abb8e23e57bea02bcc9d8a9ab40262
carl69 <carlmorgenstierne@yahoo.com>
2017-09-27 13:49:46 +0200

    change keys for drinking

--- SPO2/Assets/Scripts/Player/PlayerWater.cs
+++ SPO2/Assets/Scripts/Player/PlayerWater.cs
@@ -43,7 +43,7 @@ public class PlayerWater : MonoBehaviour
             drinkAudio = false;
         }
 
-        if (drinking == true && Input.GetKey(KeyCode.Space))
+        if (drinking == true && Input.GetKey(KeyCode.S))
         {
             if (drink < MaxWater)
             {
