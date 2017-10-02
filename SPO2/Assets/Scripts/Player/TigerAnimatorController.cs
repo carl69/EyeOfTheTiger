c14fa598f4d997516b731894e17b1d97861d07b2
Jaxhon <Jaxhon@users.noreply.github.com>
2017-10-02 16:05:32 +0200

    New trees and tiger animations

--- SPO2/Assets/Scripts/Player/TigerAnimatorController.cs
+++ SPO2/Assets/Scripts/Player/TigerAnimatorController.cs
@@ -12,10 +12,24 @@ public class TigerAnimatorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Tiger.GetComponent<Movement>().isGrounded==true)anim.SetBool("isGrounded", false);
-        else anim.SetBool("isGrounded", true);
+        if(Tiger.GetComponent<Movement>().isGrounded==true)anim.SetBool("IsGrounded", true);
+        else anim.SetBool("IsGrounded", false);
+
+        if (Tiger.GetComponent<PlayerWater>().drinkAudio == true) anim.SetBool("IsDrinking", true);
+        else anim.SetBool("IsDrinking", false);
 
         anim.SetFloat("xMovement", Tiger.GetComponent<Movement>().xMovement);
         anim.SetFloat("yMovement", Tiger.GetComponent<Movement>().yMovement);
+
+        print(Tiger.GetComponent<PlayerWater>().drinkAudio);
+        /*if (Input.GetKeyDown(KeyCode.W))
+            transform.forward = new Vector3(0f, 0f, 1f);
+        else if (Input.GetKeyDown(KeyCode.S))
+            transform.forward = new Vector3(0f, 0f, -1f);
+            */
+        if (Tiger.GetComponent<Movement>().xMovement<0)
+            transform.forward = new Vector3(-1f, 0f, 0f);
+        else if (Tiger.GetComponent<Movement>().xMovement > 0)
+            transform.forward = new Vector3(1f, 0f, 0f);
     }
 }
