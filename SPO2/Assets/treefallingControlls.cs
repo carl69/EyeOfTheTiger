033b79e2a66fd67d45a2b230093681870e57b542
carl69 <carlmorgenstierne@yahoo.com>
2017-10-04 12:37:39 +0200

    trees falling on P in the sceene

---
+++ SPO2/Assets/treefallingControlls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treefallingControlls : MonoBehaviour {
+
+    public Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            anim.enabled = true;
+
+        }
+        if (Input.GetKey(KeyCode.B))
+        {
+            
+        }
+    }
+}
