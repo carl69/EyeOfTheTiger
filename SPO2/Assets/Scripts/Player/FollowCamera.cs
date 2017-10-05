ddb2a25fc4229533c1bcbe652ee0e64dcc553d8f
carl69 <carlmorgenstierne@yahoo.com>
2017-10-05 15:22:35 +0200

    Horizontal

--- SPO2/Assets/Scripts/Player/FollowCamera.cs
+++ SPO2/Assets/Scripts/Player/FollowCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour {
 	//public GameObject Tiger;
 	public float Smooth = 10.0f;
-	float counter = 0.0f;
+	public float counter = 0.0f;
 	float x;//,y;
 
 	// Use this for initialization
@@ -20,36 +20,37 @@ public class FollowCamera : MonoBehaviour {
 
 	void FixedUpdate () {
 
-        //if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
-        //{
-        //    if (counter <= 23) counter++;
-        //    x = (GameObject.FindGameObjectWithTag("Player").transform.position.x);
+        if (Input.GetAxis("Horizontal") > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (counter <= 23) counter++;
+            x = (GameObject.FindGameObjectWithTag("Player").transform.position.x);
 
-        //    Vector3 aux2 = new Vector3(x, transform.position.y, transform.position.z);
-        //    transform.position = aux2;
-        //    transform.localRotation = Quaternion.Euler(0.0f, counter, 0.0f);
+            Vector3 aux2 = new Vector3(x, transform.position.y, transform.position.z);
+            transform.position = aux2;
+            transform.localRotation = Quaternion.Euler(0.0f, counter, 0.0f);
 
-        //}
-        //else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
-        //{
-        //    if (counter <= 23) counter++;
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (counter <= 23) counter++;
 
-        //    x = (GameObject.FindGameObjectWithTag("Player").transform.position.x);
+            x = (GameObject.FindGameObjectWithTag("Player").transform.position.x);
 
-        //    Vector3 aux3 = new Vector3(x, transform.position.y, transform.position.z);
-        //    transform.position = aux3;
-        //    transform.localRotation = Quaternion.Euler(0.0f, -counter, 0.0f);
+            Vector3 aux3 = new Vector3(x, transform.position.y, transform.position.z);
+            transform.position = aux3;
+            transform.localRotation = Quaternion.Euler(0.0f, -counter, 0.0f);
 
-        //}
-        //else {
-			if(counter >0)counter-=5;
+        }
+        else
+        {
+            if (counter >0)counter-=5;
 			if(counter <=0) counter = 0 ;
 			x = transform.position.x + (GameObject.FindGameObjectWithTag ("Player").transform.position.x - transform.position.x) / Smooth;
 
 			Vector3 aux = new Vector3 (x, transform.position.y, transform.position.z);
 			transform.position = aux;
 			transform.localRotation = Quaternion.Euler(0.0f, counter, 0.0f);
-		//}
+		}
 	}
 }
 
