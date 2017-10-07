4b73fc48233e822153bc8f47896e62d8cf8b1547
carl69 <carlmorgenstierne@yahoo.com>
2017-10-07 17:47:15 +0200

    Random pray walking

---
+++ SPO2/Assets/AISpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpawner : MonoBehaviour {
+    public GameObject pray;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("");
+
+            var gos = Instantiate(pray);
+            gos.transform.parent = this.gameObject.transform;
+            gos.transform.position = this.transform.position;
+        }
+	}
+}
