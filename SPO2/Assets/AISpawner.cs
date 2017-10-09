27d3e3d33d8392f52d95045f3ba4adc901d309d9
carl69 <carlmorgenstierne@yahoo.com>
2017-10-09 16:48:59 +0200

    aiPraySpawner only spawn 1 pray each day

--- SPO2/Assets/AISpawner.cs
+++ SPO2/Assets/AISpawner.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class AISpawner : MonoBehaviour {
     public GameObject pray;
+    Clock_24Hour Clock24Hour;
+    private GameObject Clock;
+    private bool spawnOne;
 	// Use this for initialization
 	void Start () {
-        
+        Clock = GameObject.Find("24HourClock");
+        Clock24Hour = Clock.GetComponent<Clock_24Hour>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.S))
+        if (Clock24Hour.isDay == true && spawnOne)
         {
-
+            spawnOne = false;
 
             var gos = Instantiate(pray);
             gos.transform.parent = this.gameObject.transform;
             gos.transform.position = this.transform.position;
+        }
+        if (Clock24Hour.isNight == true)
+        {
+            spawnOne = true;
         }
 	}
 }
