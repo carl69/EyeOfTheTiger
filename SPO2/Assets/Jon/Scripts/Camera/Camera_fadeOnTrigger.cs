dca20c41b144f0df047bff6c51e872de3d63a873
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-10-09 17:24:58 +0200

    Made a camera fade in/out script.

---
+++ SPO2/Assets/Jon/Scripts/Camera/Camera_fadeOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_fadeOnTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFade>().fadeDir = 1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
