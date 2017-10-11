78f77097da23e6fa970f50f4609357b922554811
carl69 <carlmorgenstierne@yahoo.com>
2017-10-11 16:14:41 +0200

    Ranger now works ;)

--- SPO2/Assets/Scripts/AI/ParkRanger/ParkRanger.cs
+++ SPO2/Assets/Scripts/AI/ParkRanger/ParkRanger.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ParkRanger : MonoBehaviour
 {
+     float dist;
 
     private Rigidbody rb;
-    public float velocity = 5, distance2 = 10, distance3 = 10, velocity2 = -5;
-	bool escapeRight = false, escapeLeft = false, unavez = false, unavez2 = false;
+     float velocity = 5, distance2 = 10, distance3 = 10, velocity2 = -5;
+    bool escapeRight = false, escapeLeft = false, unavez = false, unavez2 = false;
     public GameObject tigre;
-    public float distance = 20, mayor0 = 0.0f;
+    public float distance = 20, mayor0 = 10.0f;
     public GameObject[] players;
     int counter = 0;
 
@@ -34,39 +35,53 @@ public class ParkRanger : MonoBehaviour
     }
     void Update()
     {
+        dist = Vector2.Distance(tigre.transform.position, this.transform.position);
+        if (dist < mayor0)
+        {
+            velocity2 = 0;
+            velocity = 0;
+        }else
+        {
+            velocity2 = -5;
+            velocity = 5;
+        }
 
 
         if (escapeRight /*&& (transform.position.x > tigre.transform.position.x)*/)
         {
-            print("3");
+            //escapeLeft = false;
+            //print("3");
 			if (!unavez)
             {
-                print("5");
+                //print("5");
                 players[counter] = tigre;
                 counter++;
                 unavez = true;
             }
-			print (+transform.position.x - tigre.transform.position.x);
-			if (+transform.position.x - tigre.transform.position.x >= mayor0/* distance2*/){
-				print ("7");
+			//print (+transform.position.x - tigre.transform.position.x);
+			if (+transform.position.x - tigre.transform.position.x > -2/* distance2*/){
+				//print ("7");
 				rb.velocity = new Vector2 (velocity2, rb.velocity.y);
+               
 				//transform.position = Vector2.MoveTowards (transform.position, tigre.transform.position, velocity * Time.deltaTime);
 			}
         }
         if (escapeLeft /*&& (transform.position.x < tigre.transform.position.x)*/)
         {
-            print("4");
+            //escapeRight = false;
+            //print("4");
 			if (!unavez2)
             {
-                print("6");
+                //print("6");
                 players[counter] = tigre;
                 counter++;
                 unavez2 = true;
             }
-			if (-transform.position.x + tigre.transform.position.x >= mayor0/*distance3*/ )
+			if (-transform.position.x + tigre.transform.position.x > 2/*distance3*/ )
             {
-                print("8");
+                //print("8");
                 rb.velocity = new Vector2(velocity, rb.velocity.y);
+                
                 //transform.position = Vector2.MoveTowards (transform.position, tigre.transform.position, -1 * velocity2 * Time.deltaTime);
 			}
         }
@@ -75,16 +90,17 @@ public class ParkRanger : MonoBehaviour
 
     void checkDistancia()
     {
+
         //The part comment is if the game is in negative part
-        if (!escapeRight && /*(transform.position.x < 0 && +transform.position.x - tigre.transform.position.x <= distance && +transform.position.x - tigre.transform.position.x >= mayor0) ||*/ (transform.position.x > 0 &&  transform.position.x - tigre.transform.position.x <= distance  &&  transform.position.x - tigre.transform.position.x >= mayor0))
+        if (!escapeRight &&  (transform.position.x > 0 &&  transform.position.x - tigre.transform.position.x <= distance  &&  transform.position.x - tigre.transform.position.x >= mayor0))
         {
             escapeRight = true;
-            print("1");
+            //print("1");
         }
-        if (!escapeLeft &&/* (transform.position.x < 0 && -transform.position.x + tigre.transform.position.x <= distance && -transform.position.x + tigre.transform.position.x >= mayor0) ||*/ (transform.position.x > 0 && -transform.position.x + tigre.transform.position.x <= distance && -transform.position.x + tigre.transform.position.x >= mayor0 ))
+        if (!escapeLeft && (transform.position.x > 0 && -transform.position.x + tigre.transform.position.x <= distance && -transform.position.x + tigre.transform.position.x >= mayor0 ))
         {
             escapeLeft = true;
-            print("2");
+            //print("2");
         }
     }
 
