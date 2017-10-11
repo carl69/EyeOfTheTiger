3f44b5fcc4fcc27e0649e8cc34077d6171e88816
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-10-11 23:37:19 +0200

    Tutorial updates

--- SPO2/Assets/Jon/Scripts/ParkRangerMovement_test.cs
+++ SPO2/Assets/Jon/Scripts/ParkRangerMovement_test.cs
@@ -15,15 +15,15 @@ public class ParkRangerMovement_test : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Cub")
-        {
-            cubPos = GameObject.FindGameObjectWithTag("Cub").transform;          
-            cubSpotted = true;            
-        }
-        if(other.gameObject.tag == "Player")
-        {
-            transform.GetChild(1).gameObject.SetActive(true);
-        }
+        //if(other.gameObject.tag == "Cub")
+        //{
+        //    cubPos = GameObject.FindGameObjectWithTag("Cub").transform;          
+        //    cubSpotted = true;            
+        //}
+        //if(other.gameObject.tag == "Player")
+        //{
+        //    transform.GetChild(1).gameObject.SetActive(true);
+        //}
         if(other.gameObject.tag == "Cub")
         {
             transform.GetChild(1).transform.GetChild(0).gameObject.SetActive(true);
---
+++ SPO2/Assets/Jon/Scripts/Tutorials/Trigger_scarePoachers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_scarePoachers : MonoBehaviour {
+
+    public Sprite ParkRangerTalkie;
+    public Transform hunterTarget;
+    public float speed;
+    public bool chasePoacher = false;
+    public GameObject WalkieTalkieSprite;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ParkRanger")
+        {
+            WalkieTalkieSprite.SetActive(true);
+            other.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = ParkRangerTalkie;
+            chasePoacher = true;
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void Update () {
+       
+        if(chasePoacher == true)
+        {
+            //Debug.Log("chasePoacher is true!");
+            //float step = speed * Time.deltaTime;
+            //GameObject.FindGameObjectWithTag("Enemie").transform.position = Vector3.MoveTowards(transform.position, hunterTarget.position, step);
+            //Debug.Log("Hunter moving!");
+        }
+
+	}
+}
