5ed0e76c1401d9868ff8b527080a676b2894e165
Luismu02 <luismu02@ucm.es>
2017-10-18 11:41:31 +0200

    bulldozer stop when it arrives at a hive

--- SPO2/Assets/Scripts/DestroyTigersDen.cs
+++ SPO2/Assets/Scripts/DestroyTigersDen.cs
@@ -1,28 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyTigersDen : MonoBehaviour {
-    private GameObject workingDen;
-    private GameObject DestroyedDen;
+	private GameObject workingDen;
+	private GameObject DestroyedDen;
+	WallOfDeathScript stopedBull;
+	public bool stop = true;
+
+	public int day, day2;
+	public GameObject clock;
+	public Clock_24Hour clockScript;
+	public bool haveClockInTheScene;
 
 	// Use this for initialization
 	void Start () {
-        workingDen = this.gameObject.transform.GetChild(0).gameObject;
-        DestroyedDen = this.gameObject.transform.GetChild(1).gameObject;
-    }
+		workingDen = this.gameObject.transform.GetChild(0).gameObject;
+		DestroyedDen = this.gameObject.transform.GetChild(1).gameObject;
 
-    // Update is called once per frame
-    void Update () {
-		
+
+
+		clock = GameObject.FindGameObjectWithTag("Clock");
+		clockScript = clock.GetComponent<Clock_24Hour>();
 	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "WallofDeath")
+	// Update is called once per frame
+	void Update () {
+		day = clockScript.day;
+		if (!stop && day == day2) {
+			workingDen.SetActive(false);
+			DestroyedDen.SetActive(true);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		/*if (other.tag == "WallofDeath" && !destroyer && day == day2)
         {
             workingDen.SetActive(false);
             DestroyedDen.SetActive(true);
-        }
-    }
+        }*/
+		if (other.gameObject.tag == "WallofDeath") {
+			stop = false;
+			day2 = clockScript.day + 1;
+		}
+	}
 }
--- SPO2/Assets/Scripts/WallOfDeathScript.cs
+++ SPO2/Assets/Scripts/WallOfDeathScript.cs
@@ -15,6 +15,11 @@ public class WallOfDeathScript : MonoBehaviour {
     public Clock_24Hour clockScript;
     public bool notDriveAtNight = false;
     public bool haveClockInTheScene;
+
+	public bool stop = false;
+	public int day, day2;
+
+
     // Use this for initialization
     void Start () {
         if (!clock && haveClockInTheScene)
@@ -23,18 +28,21 @@ public class WallOfDeathScript : MonoBehaviour {
             clockScript = clock.GetComponent<Clock_24Hour>();
         }
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        
+		day = clockScript.day;
         timeLeft -= Time.deltaTime;
         if (timeLeft < 0)
         {
+			if (stop && day == day2) {
+				stop = false;
+			}
             if (notDriveAtNight)
             {
-                if (clockScript.isDay)
+				if (clockScript.isDay && !stop)
                 {
-                    if (hasStarted == false)
+                    if (hasStarted == false )
                     {
                         GameObject.Find("DeforestationStart").GetComponent<Deforestation_start>().playHorn = true;
                         hasStarted = true;
@@ -49,19 +57,29 @@ public class WallOfDeathScript : MonoBehaviour {
             }
             else
             {
-                if (hasStarted == false)
-                {
-                    GameObject.Find("DeforestationStart").GetComponent<Deforestation_start>().playHorn = true;
-                    hasStarted = true;
-                }
-                transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y, transform.position.z);
-                if (isPlayingAudio == false)
-                {
-                    GetComponent<AudioSource>().Play();
-                    isPlayingAudio = true;
-                }
+				if (!stop) {
+					if (hasStarted == false) {
+						GameObject.Find ("DeforestationStart").GetComponent<Deforestation_start> ().playHorn = true;
+						hasStarted = true;
+					}
+					transform.position = new Vector3 (transform.position.x + Time.deltaTime * speed, transform.position.y, transform.position.z);
+					if (isPlayingAudio == false) {
+						GetComponent<AudioSource> ().Play ();
+						isPlayingAudio = true;
+					}
+				}
             }
         }
 	}
     
+
+
+	public void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "TigersDen") {
+			stop = true;
+			day2 = clockScript.day + 1;
+		}
+
+	}
+
 }
