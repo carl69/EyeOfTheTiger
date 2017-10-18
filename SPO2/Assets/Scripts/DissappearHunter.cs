1c3398ef92c1514652f5e8e1ac313d09b308e830
Luismu02 <luismu02@ucm.es>
2017-10-18 20:01:19 +0200

    hunters spawn at day and despawn at night

---
+++ SPO2/Assets/Scripts/DissappearHunter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DissappearHunter : MonoBehaviour {
+
+	public float speed = 1;
+	public float timeLeft = 5;
+
+	public bool isPlayingAudio = false;
+	private bool hasStarted = false;
+
+	public GameObject clock;
+	public Clock_24Hour clockScript;
+	public bool haveClockInTheScene;
+
+	public bool dia = false;
+
+	float x,y,z;
+	// Use this for initialization
+	void Start () {
+
+		if (!clock && haveClockInTheScene)
+		{
+			clock = GameObject.FindGameObjectWithTag("Clock");
+			clockScript = clock.GetComponent<Clock_24Hour>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if (timeLeft < 0)
+		{
+			if (clockScript.isDay) {
+				dia = true;
+
+			} else {
+				
+				Destroy (this.gameObject);
+			}
+			} 
+		if (!clockScript.isDay) {
+			Destroy (this.gameObject);
+		}
+	}
+
+
+}
