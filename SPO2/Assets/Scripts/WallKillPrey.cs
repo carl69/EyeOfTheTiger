f68e478ddf3b537e6a4fa8a32b6fb8c0ce3dbaae
Luismu02 <luismu02@ucm.es>
2017-10-18 20:26:01 +0200

    Pray spawners die depending on how far away the wall of death

---
+++ SPO2/Assets/Scripts/WallKillPrey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallKillPrey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "PreySpawnerDied") {
+			Destroy (this.gameObject);
+		}
+
+	}
+}
