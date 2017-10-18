1bbb5b05b4b9c942ce94f443a56ccc2ede34f850
carl69 <carlmorgenstierne@yahoo.com>
2017-10-18 19:12:21 +0200

    uodate

---
+++ SPO2/Assets/ObjectSpawnerWhenActivated.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnerWhenActivated : MonoBehaviour {
+    public GameObject Spawn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Instantiate(Spawn, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        this.gameObject.SetActive(false);
+	}
+    
+}
