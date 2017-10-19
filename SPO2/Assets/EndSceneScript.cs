ac0438aa6cbf307a352096e33cff369d02b4481f
carl69 <carlmorgenstierne@yahoo.com>
2017-10-19 16:10:16 +0200

    EndScene buttons works

---
+++ SPO2/Assets/EndSceneScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneScript : MonoBehaviour {
+
+
+    public void Restart() {
+        SceneManager.LoadScene(1);
+    }
+    public void GoTOMainMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
