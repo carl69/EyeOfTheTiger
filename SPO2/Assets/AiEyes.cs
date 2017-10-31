88ce2ed463a7167b5436196d51d65ade80bc455e
carl69 <carlmorgenstierne@yahoo.com>
2017-10-31 13:08:27 +0100

    increase the wall of death size (+ a lot more)

--- SPO2/Assets/AiEyes.cs
+++ SPO2/Assets/AiEyes.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class AiEyes : MonoBehaviour
 {
+    public GameObject test;
+
+
     public float maxEyeSight = 25;
     public LayerMask LayersThatCanBeSeen;
 
     private void FixedUpdate()
     {
-        Ray eyes = new Ray(transform.position, Vector3.forward);
+        Ray eyes = new Ray(transform.position, test.transform.position/*new Vector3(0,transform.rotation.y / 360,1)*/);
         RaycastHit[] hits = Physics.RaycastAll(eyes, maxEyeSight, LayersThatCanBeSeen);
 
-        Debug.DrawLine(transform.position, transform.position + Vector3.forward * maxEyeSight, Color.green);
+        Debug.Log(hits[0].transform.gameObject);
+        Debug.DrawLine(transform.position, test.transform.position/*transform.position + (Vector3.forward * maxEyeSight)*/ , Color.green);
 
         foreach(RaycastHit hit in hits)
         {
