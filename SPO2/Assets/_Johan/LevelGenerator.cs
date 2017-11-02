ddef62318276ed6b65201aa81e4009937a3bcf2c
Jaxhon <Jaxhon@users.noreply.github.com>
2017-11-02 17:04:03 +0100

    generation

--- SPO2/Assets/_Johan/LevelGenerator.cs
+++ SPO2/Assets/_Johan/LevelGenerator.cs
@@ -3,88 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
+    public int whatModuleWasSupposedToBeLoaded;
     public int levelLength;
     public int difficultyLevel;
 
     public int whatKindOfModueShouldBeLoaded;
-    public GameObject[] modules;
 
+    GameObject[] startingModules;
+    GameObject[] finishingModules;
 
-    public GameObject[] startingModules;
-    public GameObject[] finishingModules;
-
-    public GameObject[] denModules;
     public GameObject[] poacherModules;
     public GameObject[] resourceModules;
-    
+
+    public int poacherChance;
+    public int resourceChance;
 
     // Use this for initialization
     void Start () {
-        //modules = Resources.LoadAll<GameObject>("Modules") as GameObject[];
-        //Instantiate(modules[Random.Range(0, modules.Length)], gameObject.transform.position, gameObject.transform.rotation);
         GenerateNextModule();
     }
 
     public void GenerateNextModule()
     {
-        whatKindOfModueShouldBeLoaded = Random.Range(1, 100);
         if (difficultyLevel == 1)
         {
-            denModules = Resources.LoadAll<GameObject>("Modules/Difficulty1/Den") as GameObject[];
             poacherModules = Resources.LoadAll<GameObject>("Modules/Difficulty1/Poacher") as GameObject[];
             resourceModules = Resources.LoadAll<GameObject>("Modules/Difficulty1/Resources") as GameObject[];
-
-            if (whatKindOfModueShouldBeLoaded <= 10 && whatKindOfModueShouldBeLoaded >= 1)
-            {
-                Instantiate(denModules[Random.Range(0, denModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded >= 11 && whatKindOfModueShouldBeLoaded <= 40)
-            {
-                Instantiate(poacherModules[Random.Range(0, poacherModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded >= 41 && whatKindOfModueShouldBeLoaded <= 100)
-            {
-                Instantiate(resourceModules[Random.Range(0, resourceModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-
         }
         if (difficultyLevel == 2)
         {
-            denModules = Resources.LoadAll<GameObject>("Modules/Difficulty2/Den") as GameObject[];
             poacherModules = Resources.LoadAll<GameObject>("Modules/Difficulty2/Poacher") as GameObject[];
             resourceModules = Resources.LoadAll<GameObject>("Modules/Difficulty2/Resources") as GameObject[];
-
-            if (whatKindOfModueShouldBeLoaded <= 10 && whatKindOfModueShouldBeLoaded >= 1)
-            {
-                Instantiate(denModules[Random.Range(0, denModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded <= 11 && whatKindOfModueShouldBeLoaded >= 40)
-            {
-                Instantiate(poacherModules[Random.Range(0, poacherModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded <= 41 && whatKindOfModueShouldBeLoaded >= 100)
-            {
-                Instantiate(resourceModules[Random.Range(0, resourceModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
         }
         if (difficultyLevel == 3)
         {
-            denModules = Resources.LoadAll<GameObject>("Modules/Difficulty3/Den") as GameObject[];
             poacherModules = Resources.LoadAll<GameObject>("Modules/Difficulty3/Poacher") as GameObject[];
             resourceModules = Resources.LoadAll<GameObject>("Modules/Difficulty3/Resources") as GameObject[];
+        }
 
-            if (whatKindOfModueShouldBeLoaded <= 10 && whatKindOfModueShouldBeLoaded >= 1)
-            {
-                Instantiate(denModules[Random.Range(0, denModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded <= 11 && whatKindOfModueShouldBeLoaded >= 40)
-            {
-                Instantiate(poacherModules[Random.Range(0, poacherModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
-            if (whatKindOfModueShouldBeLoaded <= 41 && whatKindOfModueShouldBeLoaded >= 100)
-            {
-                Instantiate(resourceModules[Random.Range(0, resourceModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
-            }
+        whatKindOfModueShouldBeLoaded = Random.Range(1, poacherChance + resourceChance);
+
+            
+        //choose type of module
+        if (whatKindOfModueShouldBeLoaded <= poacherChance)
+        {
+            whatModuleWasSupposedToBeLoaded = Random.Range(0, poacherModules.Length);
+            Instantiate(poacherModules[Random.Range(0, poacherModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
+            print("paochieros in sight");
+        }
+        else if (whatKindOfModueShouldBeLoaded > poacherChance)
+        {
+            whatModuleWasSupposedToBeLoaded = Random.Range(0, resourceModules.Length);
+            Instantiate(resourceModules[Random.Range(0, resourceModules.Length)], gameObject.transform.position, gameObject.transform.rotation);
+            print("Water! ..... or food...");
         }
     }
 }
