1166ef29ea3bbcaa39fffdc2b04f747cea09e40d
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-11-02 20:20:23 +0100

    Working on world map.

---
+++ SPO2/Assets/_Jon/Scripts/WorldMap/WorldMap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour {
+
+    public float speed;
+    public int food = 0;
+    public bool cubLevel03 = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Node01")
+            {
+                Debug.Log("Hovering over first waterfall");
+                GameObject.Find("Node01").transform.GetChild(2).gameObject.SetActive(true);
+               
+            }
+            else
+            {
+                GameObject.Find("Node01").transform.GetChild(2).gameObject.SetActive(false);
+            }
+         
+        }
+            if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray02 = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit02;
+            if(Physics.Raycast(ray02, out hit02))
+            {
+                if(hit02.transform.name == "Node01")
+                {
+                    Debug.Log("This is the first waterfall");
+                    GameObject.FindGameObjectWithTag("Player").transform.position = Vector3.Lerp(GameObject.FindGameObjectWithTag("Player").transform.position, GameObject.Find("Node01").transform.GetChild(0).transform.position, step);
+                }
+                if (hit02.transform.name == "Node02")
+                {
+                    Debug.Log("This is the first clearing");
+                    GameObject.FindGameObjectWithTag("Player").transform.position = Vector3.Lerp(GameObject.FindGameObjectWithTag("Player").transform.position, GameObject.Find("Node02").transform.GetChild(0).transform.position, step);
+                }
+                if (hit02.transform.name == "Node03")
+                {
+                    Debug.Log("This is the first poacher camp");
+                    GameObject.FindGameObjectWithTag("Player").transform.position = Vector3.Lerp(GameObject.FindGameObjectWithTag("Player").transform.position, GameObject.Find("Node03").transform.GetChild(0).transform.position, step);
+                }
+                if (hit02.transform.name == "Den01")
+                {
+                    Debug.Log("This is the first den");
+                    GameObject.FindGameObjectWithTag("Player").transform.position = Vector3.Lerp(GameObject.FindGameObjectWithTag("Player").transform.position, GameObject.Find("Den01").transform.GetChild(0).transform.position, step);
+                }
+            }
+        }
+	}
+}
