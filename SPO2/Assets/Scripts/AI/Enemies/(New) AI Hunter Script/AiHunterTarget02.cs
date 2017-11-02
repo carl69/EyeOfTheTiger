07af231c3cb7c1991ef6380da908b273f7767ef4
carl69 <carlmorgenstierne@yahoo.com>
2017-11-02 23:33:02 +0100

    new hunter in the works

---
+++ SPO2/Assets/Scripts/AI/Enemies/(New) AI Hunter Script/AiHunterTarget02.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiHunterTarget02 : MonoBehaviour {
+
+    public GameObject Target;
+    public List<GameObject> Targets = new List<GameObject>();
+    AiHunters02 aihunter02;
+
+
+    float UpdateTimer = 2;
+    float curTime = 2;
+
+    private void Start()
+    {
+        aihunter02 = GetComponent<AiHunters02>();
+    }
+    private void Update()
+    {
+        if (Targets[0] == null)
+        {
+            Target = null;
+            Targets.Remove(Targets[0]);
+        }
+        if (Targets.Count != 0 )
+        {
+            
+            if (Targets[0] != null)
+            {
+                if (aihunter02.fpsTarget != Target.transform)
+                {
+                    aihunter02.fpsTarget = Target.transform;
+                }
+            }
+            
+        }
+        
+    }
+
+
+    private void OnTriggerEnter(Collider x)
+    {
+        if ((x.tag == "Player" || x.tag == "Pray") && !Targets.Contains(x.gameObject))
+        {
+            Targets.Add(x.gameObject);
+
+            if (Target == null)
+            {
+                print("problem is here");
+                Target = x.gameObject;
+            }
+            for (int i = 0; i < Targets.Count; i++)
+            {
+                if (Targets[i] == null)
+                {
+                    Targets.Remove(Targets[i]);
+                }
+                if (Targets[i].tag == "Player")
+                {
+                    Target = Targets[i];
+                }
+            }
+            
+        }
+    }
+    private void OnTriggerExit(Collider d)
+    {
+        if (d.tag == "Player" || d.tag == "Pray")
+        {
+            Targets.Remove(d.gameObject);
+
+
+            if (d.tag == "Player")
+            {
+                if (Targets.Count <= 2)
+                {
+                    if (Target != Targets[Targets.Count - 1])
+                    {
+                        Target = Targets[Targets.Count - 1];
+                    }
+                    else
+                    {
+                        Target = Targets[Targets.Count - 2];
+                    }
+                }
+            }
+
+            Targets.Remove(d.gameObject);
+
+
+        }
+    }
+
+
+}
---
+++ SPO2/Assets/Scripts/AI/Enemies/(New) AI Hunter Script/AiHunters02.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiHunters02 : MonoBehaviour {
+    [Range(30f, 0f)]
+    public float fpsTargetDistance;
+    [Range(30f, 0f)]
+    public float enemyLookDistance;
+    [Range(30f, 0f)]
+    public float moveToAttackDistance;
+    [Range(30f, 0f)]
+    public float shootDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+    public Transform fpsTarget;
+    Rigidbody rb;
+    Renderer myRenderer;
+    AiHunterShooting aihunterShooting;
+    AiHunterIdel aihunteridel;
+    // Use this for initialization
+    void Start () {
+        myRenderer = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
+        aihunterShooting = GetComponent<AiHunterShooting>();
+        aihunteridel = GetComponent<AiHunterIdel>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (fpsTarget != null)
+        {
+            fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+
+            if (fpsTargetDistance < moveToAttackDistance)
+            {
+
+                attack();
+                lookAtPlayer();
+
+                print("Attack");
+            }
+            else
+                if (fpsTargetDistance < enemyLookDistance)
+            {
+                myRenderer.material.color = Color.yellow;
+                lookAtPlayer();
+
+                if (aihunteridel.enabled == true)
+                {
+                    aihunteridel.enabled = false;
+                }
+                if (aihunterShooting.enabled == true)
+                {
+                    aihunterShooting.enabled = false;
+                }
+                print("Look at the player");
+            }
+            else
+            {
+
+                if (aihunteridel.enabled == false)
+                {
+                    aihunteridel.enabled = true;
+                }
+                if (aihunterShooting.enabled == true)
+                {
+                    aihunterShooting.enabled = false;
+                }
+                myRenderer.material.color = Color.white;
+            }
+        }
+        else
+        {
+            if (aihunteridel.enabled == false)
+            {
+                aihunteridel.enabled = true;
+            }
+            if (aihunterShooting.enabled == true)
+            {
+                aihunterShooting.enabled = false;
+            }
+            myRenderer.material.color = Color.white;
+        }
+	}
+
+    void lookAtPlayer()
+    {
+        Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+    }
+    void attack()
+    {
+        if (fpsTargetDistance < shootDistance)
+        {
+            myRenderer.material.color = Color.black;
+            if (aihunterShooting.enabled == false)
+            {
+                aihunterShooting.enabled = true;
+            }
+            print("shoot");
+        }
+        else
+        {
+            myRenderer.material.color = Color.red;
+            transform.position += (transform.forward * enemyMovementSpeed * Time.deltaTime);
+        }
+    }
+}
