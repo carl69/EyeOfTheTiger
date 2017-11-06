d6351a96c796fb906a96647d9072c8dd8e60e2c7
carl69 <carlmorgenstierne@yahoo.com>
2017-11-06 15:48:17 +0100

    foodrack update

--- SPO2/Assets/Scripts/Player/food.cs
+++ SPO2/Assets/Scripts/Player/food.cs
@@ -9,7 +9,8 @@ public class food : MonoBehaviour {
     //[HideInInspector]
     public bool eating = false;
     public float eaten = 50;
-    private float amountOfFood = 20;
+    [HideInInspector]
+    public float amountOfFood = 20;
     // food loss rate
     private float rate;
     private float timer;
---
+++ SPO2/Assets/Scripts/WorldObjects/FoodRack/FoodRackScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodRackScript : MonoBehaviour {
+    public int amountOfFood;
+    bool canTakeFood = false;
+    food Food;
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Food = player.GetComponent<food>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canTakeFood)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (amountOfFood >= 1)
+                {
+                    if (Food.maxFood - Food.eaten <= Food.amountOfFood)
+                    {
+                        Food.eaten = Food.maxFood;
+                    }
+                    else
+                    {
+                        Food.eaten += Food.amountOfFood;
+                    }
+                    
+                    amountOfFood -= 1;
+                }
+                else
+                {
+                    print(this.gameObject.name + " is out of food :(");
+                }
+            }
+        }
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canTakeFood = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canTakeFood = false;
+        }
+    }
+}
