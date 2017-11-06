df82d24334182ffb697fbc8c455e4c6f4447f619
carl69 <carlmorgenstierne@yahoo.com>
2017-11-06 14:01:05 +0100

    implementing player prefs

--- SPO2/Assets/_Jon/Scripts/WorldMap/WorldMapTransitionTo.cs
+++ SPO2/Assets/_Jon/Scripts/WorldMap/WorldMapTransitionTo.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WorldMapTransitionTo : MonoBehaviour {
-
+    int days;
 	// Use this for initialization
 	void Start () {
-		
+        days = PlayerPrefs.GetInt("Days");
 	}
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            days += 1;
+            PlayerPrefs.SetInt("Days",days);
             Debug.Log("Player entered trigger!");
             SceneManager.LoadScene("WorldMap");
         }
