35192dd45da2bcd9a8487f192ce2203720ca3154
carl69 <carlmorgenstierne@yahoo.com>
2017-11-08 16:25:20 +0100

    tiger camera smoothness

--- SPO2/Assets/Scripts/Camera/cameraTargetFollow.cs
+++ SPO2/Assets/Scripts/Camera/cameraTargetFollow.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraTargetFollow : MonoBehaviour {
-
+    public float curentDistance;
+    public float maxDistance;
+    public float speed;
+    public float timeBeforeIdel;
+    public float timer;
+    public float leftRight;
 	// Use this for initialization
 	void Start () {
-		
+        leftRight = Input.GetAxis("Horizontal");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (timer < timeBeforeIdel)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            leftRight = Input.GetAxis("Horizontal");
+
+            if (curentDistance < maxDistance || curentDistance > -maxDistance)
+            {
+                curentDistance = maxDistance * leftRight;
+                transform.localPosition = new Vector3(curentDistance, 0, 0);
+
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") == 1 || Input.GetAxis("Horizontal") == -1)
+        {
+            timer = 0;
+            leftRight = Input.GetAxis("Horizontal");
+            curentDistance += leftRight * speed;
+
+            if (curentDistance < maxDistance || curentDistance > -maxDistance)
+            {
+                curentDistance = maxDistance * leftRight;
+                transform.localPosition = new Vector3(curentDistance, 0, 0);
+
+            }
+        }
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
