9f2ded5a52d8c4ca338a50ebf093b9926073d4de
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-11-12 17:24:38 +0100

    Slight update to transitions between scenes.

---
+++ SPO2/Assets/_Jon/Scripts/WorldMap/Testing/TutorialController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+    public GameObject Instructions01;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PlayerPrefs.GetInt("Days") == 1)
+        {
+            Instructions01.SetActive(true);
+        }
+	}
+}
--- SPO2/Assets/_Jon/Scripts/WorldMap/WorldMapTransitionTo.cs
+++ SPO2/Assets/_Jon/Scripts/WorldMap/WorldMapTransitionTo.cs
@@ -5,24 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class WorldMapTransitionTo : MonoBehaviour {
     int days;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         days = PlayerPrefs.GetInt("Days");
-	}
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             days += 1;
-            PlayerPrefs.SetInt("Days",days);
+            PlayerPrefs.SetInt("Days", days);
             Debug.Log("Player entered trigger!");
-            SceneManager.LoadScene("WorldMap");
+            if (PlayerPrefs.GetInt("Home") == 0)
+            {
+                SceneManager.LoadScene("Tutorial_3");
+            }
+            if(PlayerPrefs.GetInt("Home") == 1)
+            {
+                SceneManager.LoadScene("Den01");
+            }
+
         }
     }
+        // Update is called once per frame
+        void Update() {
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+        }
 }
