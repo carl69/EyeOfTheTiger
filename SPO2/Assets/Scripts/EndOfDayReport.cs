cc74eaae7679b7de19dd5c57fa0cc4e0014a8144
Luismu02 <luismu02@ucm.es>
2017-11-27 17:59:31 +0100

    EndOfDaywork

---
+++ SPO2/Assets/Scripts/EndOfDayReport.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfDayReport : MonoBehaviour {
+	CarlsPlayerprefsStalkerScript datas;
+	//public int NumberFoodInPocket, NumberFoodInFridge, FoodInPocketAndFridge, FinalFoodInReport, actualizarDatas, RestInFridgeToZero, RestInFridge, RestInPocket;
+	public float NumberFoodInPocket, NumberFoodInFridge, FoodInPocketAndFridge, FinalFoodInReport, actualizarDatas, RestInFridgeToZero, RestInFridge, RestInPocket;
+	public int subtractedFood = 5;
+	// Use this for initialization
+	void Start () {
+		datas = GetComponent<CarlsPlayerprefsStalkerScript>();
+	}
+		// Update is called once per frame
+	void Update () {
+		NumberFoodInPocket = datas.Food / 10;
+		NumberFoodInFridge = datas.StoredFood;
+		print ("yep");
+		if (NumberFoodInFridge > 0 && NumberFoodInFridge <= subtractedFood) {
+			print ("hola");
+			RestInFridgeToZero = subtractedFood - NumberFoodInFridge;
+			RestInPocket = NumberFoodInPocket - RestInFridgeToZero;
+			datas.StoredFood = 0;
+			datas.Food = RestInPocket * 10;
+
+		} else if (NumberFoodInFridge > subtractedFood) {
+			print ("hola2");
+			RestInFridge = NumberFoodInFridge - subtractedFood;
+			datas.StoredFood = (int) RestInFridge;
+
+		} else if (NumberFoodInFridge == 0) {
+			print ("hola3");
+			RestInPocket = NumberFoodInPocket - subtractedFood;
+			print (datas.Food);
+			datas.Food = RestInPocket * 10;
+			print (datas.Food);
+		}
+ 
+	}
+}
