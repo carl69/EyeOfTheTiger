5a97e0184085b06432cc36417fc277e3c0abe71e
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2017-11-27 19:57:08 +0100

    Updated the endofdayreport script and set up initial UI system for displaying the maths equation when the player return to the den scene from day 1 and onwards.

--- SPO2/Assets/Scripts/EndOfDayReport.cs
+++ SPO2/Assets/Scripts/EndOfDayReport.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndOfDayReport : MonoBehaviour {
 	CarlsPlayerprefsStalkerScript datas;
 	//public int NumberFoodInPocket, NumberFoodInFridge, FoodInPocketAndFridge, FinalFoodInReport, actualizarDatas, RestInFridgeToZero, RestInFridge, RestInPocket;
 	public float NumberFoodInPocket, NumberFoodInFridge, FoodInPocketAndFridge, FinalFoodInReport, actualizarDatas, RestInPocketToZero, RestInFridge, RestInPocket;
-	public int subtractedFood = 11;
+	public int subtractedFood = 2;
+
+    public 
+
 	// Use this for initialization
 	void Start () {
 		datas = GetComponent<CarlsPlayerprefsStalkerScript>();
+        if (SceneManager.GetActiveScene().name.Contains("Den") && PlayerPrefs.GetInt("Days") > 0)
+        {
+            Equation();
+        }
+        else if (PlayerPrefs.GetInt("Days") == 0)
+        {
+            GameObject.Find("EndOfDayReport").SetActive(false);
+        }
 	}
 		// Update is called once per frame
 	public void Equation () {
-		NumberFoodInPocket = datas.Food / 10;
+
+        NumberFoodInPocket = datas.Food / 10;
 		NumberFoodInFridge = datas.StoredFood;
-		print ("yep");
+
+        GameObject.Find("EndOfDayReport").gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = NumberFoodInFridge.ToString();
+        GameObject.Find("EndOfDayReport").gameObject.transform.GetChild(0).transform.GetChild(1).GetComponent<Text>().text = NumberFoodInPocket.ToString();
+        GameObject.Find("EndOfDayReport").gameObject.transform.GetChild(0).transform.GetChild(2).GetComponent<Text>().text = subtractedFood.ToString();
+
+        print ("yep");
 		if (NumberFoodInPocket > 0 && NumberFoodInPocket <= subtractedFood) {
 			print ("hola");
 			RestInPocketToZero = subtractedFood - NumberFoodInPocket ;
@@ -38,7 +57,10 @@ public class EndOfDayReport : MonoBehaviour {
 			datas.StoredFood = (int)RestInFridge;
 			print (datas.StoredFood);
 		}
-		
- 
-	}
+       
+        GameObject.Find("EndOfDayReport").gameObject.transform.GetChild(0).transform.GetChild(3).GetComponent<Text>().text = (NumberFoodInPocket + NumberFoodInFridge - subtractedFood).ToString();
+
+
+    }
+
 }
