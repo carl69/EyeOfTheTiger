e22588a494010949b9e7046c4c64e34315025509
carl69 <carlmorgenstierne@yahoo.com>
2017-12-03 22:02:59 +0100

    Made the player look back and forth

---
+++ SPO2/Assets/RotatePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+    bool left;
+    public float speed = 20;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            left = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            left = true;
+        }
+
+        if (left == false)
+        {
+            transform.Rotate(Vector3.up * speed*Time.deltaTime, Space.World);
+        }
+	}
+}
